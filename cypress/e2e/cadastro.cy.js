58f71606abead90d358f3f33eea4ab9e9fc127df
paul0costa <paulorscosta@live.com>
2026-06-24 00:16:37 +0800

    automacao de ui com cypress parte 1 e 2

---
+++ cypress/e2e/cadastro.cy.js
@@ -0,0 +1,38 @@
+/// <reference types="cypress" />
+import { faker } from '@faker-js/faker';
+
+describe('Funcionalidade: Cadastro no Hub de Leitura', () => {
+
+    beforeEach(() => {
+        cy.visit('register.html')
+    });
+
+    it('Deve fazer um cadastro com sucesso, usando JS', () => {
+        let email = `teste${Date.now()}@teste.com`
+        cy.get('#name').type('Maria da Silva')
+        cy.get('#email').type(email)
+        cy.get('#phone').type('123456789')
+        cy.get('#password').type('Senha123')
+        cy.get('#confirm-password').type('Senha123')
+        cy.get('#terms-agreement').check()
+        cy.get('#register-btn').click()
+        //Resultado esperado
+        cy.url().should('include', 'dashboard')
+    });
+
+    it('Deve fazer um cadastro com sucesso, usando Faker', () => {
+        let nome = faker.person.fullName()
+        let email = faker.internet.email()
+        cy.get('#name').type(nome)
+        cy.get('#email').type(email)
+        cy.get('#phone').type('123456789')
+        cy.get('#password').type('Senha123')
+        cy.get('#confirm-password').type('Senha123')
+        cy.get('#terms-agreement').check()
+        cy.get('#register-btn').click()
+        //Resultado esperado
+        cy.url().should('include', 'dashboard')
+        cy.get('#user-name').should('contain', nome)
+    });
+
+});
